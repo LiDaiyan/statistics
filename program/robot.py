8354e870008b24bbe89e429dde50f70bede7d5f4
litanyue <977681262@qq.com>
2021-09-05 21:46:25 +0800

    feat:增加补卡选项

--- program/robot.py
+++ program/robot.py
@@ -28,6 +28,7 @@ def index():
     name = list[3]
     content = list[2]
     val = list[1]
+    card_type = list[0]
 
     try:
         float(val)
@@ -37,58 +38,18 @@ def index():
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
 
-    # number = datetime.datetime.now().weekday()
-
-    # today = datetime.date.today()
-    # yesterday = today - datetime.timedelta(days=1)
-    # day_befor_yesterday = today - datetime.timedelta(days=2)
-
-    # today_str = today.strftime(time_format)
-    # yesterday_str = yesterday.strftime(time_format)
-    # day_before_yesterday_str = day_befor_yesterday.strftime(time_format)
-
-    # # 判断今天是不是周日
-    # if(number == 6):
-    #     # 判断周六即昨天有没有打卡，如果没打卡就把周五当作昨天
-    #    select_sql = "select * from clock where name = \"{0}\" and date = \"{1}\"".format(name, yesterday_str)
-    #    query_result = cursor.execute(select_sql).rowcount
-    #    if(query_result == 0):
-    #        yesterday = today - datetime.timedelta(days=2)
-    #        yesterday_str = yesterday.strftime(time_format)
-
-    # # 判断今天是不是周一
-    # elif(number == 0):
-    #     # 判断周六周日有没有打卡，如果没打卡就把周五当作昨天，如果打了卡就把最后一次打卡那天当作昨天
-    #     select_sql = "select * from clock where name = \"{0}\" and (date = \"{1}\" or date = \"{2}\")".format(name, yesterday_str, day_before_yesterday_str)
-    #     query_result = cursor.execute(select_sql).fetchall()
-    #     if(len(query_result) == 0):
-    #         yesterday = today - datetime.timedelta(days=3)
-    #         yesterday_str = yesterday.strftime(time_format)
-    #     else:
-    #         yesterday_str = query_result[-1][-1]
-
-    # #判断今天该成员是否已打卡
-    # select_sql = "select * from clock where name = \"{0}\" and date = \"{1}\"".format(name, today_str)
-    # query_result = cursor.execute(select_sql).rowcount
-
-    # if(query_result != 0):
-    #     delete_sql = "delete from clock where name = \"{0}\" and date = \"{1}\"".format(name, today_str)
-    #     cursor.execute(delete_sql)
-
-    # # 更新今天计划完成情况
-    # update_sql = "update clock set val = {0} where name = \"{1}\" and date = \"{2}\"".format(val, name, yesterday_str)
-    # cursor.execute(update_sql)
-
-    # # 插入明天学习计划
-    # sql = "insert into clock values(\"{0}\", \"{1}\", null, \"{2}\")".format(name, content, today_str)
-    # cursor.execute(sql)
-
-
     today = datetime.date.today()
+    if(card_type == "补卡"):
+        today = today - datetime.timedelta(days=1)
     today_str = today.strftime(time_format)
     # 参数中有日期则使用参数日期
     if date:
         today_str = date
+        if(card_type == "补卡"):
+            today = datetime.datetime.strptime(date, time_format)
+            today = today - datetime.timedelta(days=1)
+            today_str = today.strftime(time_format)
+
     #判断今天该成员是否已打卡
     select_sql = "select * from clock where name = \"{0}\" and date = \"{1}\"".format(name, today_str)
     query_result = cursor.execute(select_sql).rowcount
